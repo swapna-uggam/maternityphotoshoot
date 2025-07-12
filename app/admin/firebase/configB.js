57efe0d72be3279875b93ec050855f8474d4129e
swapna-uggam <149858443+swapna-uggam@users.noreply.github.com>
2025-07-12 11:50:42 +0530

    Update configB.js

--- app/admin/firebase/configB.js
+++ app/admin/firebase/configB.js
@@ -1,14 +1,33 @@
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+// const firebaseConfigB = {
+//   apiKey: "AIzaSyCBT6Z8btjIOu0FAvg4eZFdN5X8rlWEQM8",
+//   authDomain: "packages-9624e.firebaseapp.com",
+//   projectId: "packages-9624e",
+//   storageBucket: "packages-9624e.firebasestorage.app",
+//   messagingSenderId: "63851835533",
+//   appId: "1:63851835533:web:d17ae7423fd2c6dc5cf2c4",
+//   measurementId: "G-CB9H4EHRG1"
+// };
+// const appB = initializeApp(firebaseConfigB,"secondary");
+// export const authB = getAuth(appB);
+// export const dbB = getFirestore(appB);
+
+// ---------------------------------------
+
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfigB = {
-  apiKey: "AIzaSyCBT6Z8btjIOu0FAvg4eZFdN5X8rlWEQM8",
-  authDomain: "packages-9624e.firebaseapp.com",
-  projectId: "packages-9624e",
-  storageBucket: "packages-9624e.firebasestorage.app",
-  messagingSenderId: "63851835533",
-  appId: "1:63851835533:web:d17ae7423fd2c6dc5cf2c4",
-  measurementId: "G-CB9H4EHRG1"
+  apiKey: "AIzaSyACwxoFXr5PJRiQhu0vK3ceJk6r-hAE_cg",
+  authDomain: "packgesforsnapu.firebaseapp.com",
+  projectId: "packgesforsnapu",
+  storageBucket: "packgesforsnapu.firebasestorage.app",
+  messagingSenderId: "754088266525",
+  appId: "1:754088266525:web:259e2dac44778e6df7aff4",
+  measurementId: "G-WJW6M1QRWL"
 };
 const appB = initializeApp(firebaseConfigB,"secondary");
 export const authB = getAuth(appB);
